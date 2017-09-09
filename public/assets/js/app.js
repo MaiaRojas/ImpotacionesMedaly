728653053fa3c605025e9318bb9e8668bf3061d2
maia rojas <maia.rt.46@gmail.com>
2017-09-08 19:32:40 -0500

    Prueba

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,4 +1,37 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-'use strict';
-console.log("hola");
+"use strict";
+const render = (root)=>{
+   root.empty();
+
+   const section = $('<section class="components">HOLI bOLI</section>');
+   root.append(section);
+
+};
+
+const state = {
+   page: 0,
+};
+
+const update = function (){
+ render(root);
+};
+
+$( _ => {
+
+  var holaMundo = document.getElementById('holaMundo');
+  var database = firebase.database();
+  database.ref().on("value", function(snap){
+
+    state.autonics = snap.val();
+    console.log(snap.val());
+  });
+
+
+ // dbRef.on('value', snap => holaMundo.innerText = snap.val() );
+
+    const root = $('#root');
+    render(root);
+
+ });
+
 },{}]},{},[1])
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,2 +1,34 @@
-'use strict';
-console.log("hola");
+"use strict";
+const render = (root)=>{
+   root.empty();
+
+   const section = $('<section class="components">HOLI bOLI</section>');
+   root.append(section);
+
+};
+
+const state = {
+   page: 0,
+};
+
+const update = function (){
+ render(root);
+};
+
+$( _ => {
+
+  var holaMundo = document.getElementById('holaMundo');
+  var database = firebase.database();
+  database.ref().on("value", function(snap){
+
+    state.autonics = snap.val();
+    console.log(snap.val());
+  });
+
+
+ // dbRef.on('value', snap => holaMundo.innerText = snap.val() );
+
+    const root = $('#root');
+    render(root);
+
+ });
